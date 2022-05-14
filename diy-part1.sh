c71c543534568d4114253c428228844a22edff93
aliass532 <56620619+aliass532@users.noreply.github.com>
2022-05-14 22:30:07 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,6 +15,7 @@
 echo "src-git PWpackages https://github.com/xiaorouji/openwrt-passwall.git;packages" >> feeds.conf.default
 echo "src-git PWluci https://github.com/xiaorouji/openwrt-passwall.git;luci" >> feeds.conf.default
 git clone https://github.com/aliass532/luci-app-eqos package/eqos
+sed -i 's/192.168.1.1/192.168.11.1/g' package/base-files/files/bin/config_generate
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
